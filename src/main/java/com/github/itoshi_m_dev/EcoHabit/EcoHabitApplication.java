c105b8bfb9eacfc13c09c0dbddb678a34bfa4248
itoshi-m-dev <emanuelkalison@gmail.com>
2025-11-24 17:55:38 -0300

    Inicio Do projeto

---
+++ src/main/java/com/github/itoshi_m_dev/EcoHabit/EcoHabitApplication.java
@@ -0,0 +1,13 @@
+package com.github.itoshi_m_dev.EcoHabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoHabitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoHabitApplication.class, args);
+	}
+
+}
